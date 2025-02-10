0c1d4f2839e3f7190767ccdc04f972ec37cfa81c
abidullah786 <abidullah.bsef18@iba-suk.edu.pk>
2025-02-10 15:24:38 +0500

    multi and nested query added

--- resolvers.js
+++ resolvers.js
@@ -9,4 +9,25 @@ export const resolvers = {
         reviews: () => db.reviews,
         review: (_, { id }) => db.reviews.find(review => review.id === id),
     },
+    // qury for game inside a review
+    Review: {
+        game(parent, { id }) {
+            return db.games?.find(g => g.id === parent.game_id)
+        },
+        author(parent, { id }) {
+            return db.authors?.find(a => a.id === parent.author_id)
+        }
+    },
+    // query for reviews inside game query
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter(r => r.game_id === parent.id)
+        }
+    },
+    // query for reviews inside game query
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(r => r.author_id === parent.id)
+        }
+    }
 }
--- schema.js
+++ schema.js
@@ -4,18 +4,22 @@ export const typeDefs = `#graphql
     }
     type Game{
         id: ID!
-        title: String!
-        platform: [String!]!
+        title: String! 
+        platform: [String!]! # this means that neither the platform list it self nor the items of the platfom cna be null.
+        reviews: [Review!] # this means the reviews as whole can be null but it it contains the list of reviews then it must not be null
     },
     type Review {
         id: ID!
         rating: Float!
         content: String!
+        author: Author!
+        game: Game!
     },
     type Author {
         id: ID!
         name: String!
         verified: Boolean! 
+        reviews: [Review!] # this means the reviews as whole can be null but it contains the list of reviews then it must not be null
     }
     type Query{
         games: [Game]
